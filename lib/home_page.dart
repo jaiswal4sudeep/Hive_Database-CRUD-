8a35dcc37fbe3fbafcb5443deb2094a2fa462f7c
Sudeep Jaiswal <51513908+jaiswal4sudeep@users.noreply.github.com>
2022-03-30 00:50:02 +0530

    incompleted update operation

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -75,6 +75,61 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  updatePopup(title, desc) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Update ToDo'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    controller: todoTitle,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      label: Text('Title'),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    controller: todoDesc,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      label: Text('Description'),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            actions: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  if (todoTitle.text.isNotEmpty && todoDesc.text.isNotEmpty) {
+                    final String title = todoTitle.text;
+                    final String description = todoDesc.text;
+                    todoTitle.clear();
+                    todoDesc.clear();
+                    ToDoModel data = ToDoModel(title, description);
+                    box.add(data);
+                    Navigator.of(context).pop();
+                  } else {
+                    // ignore: avoid_print
+                    print('Please add data');
+                  }
+                },
+                icon: const Icon(Icons.edit),
+                label: const Text('Update'),
+              ),
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -104,11 +159,26 @@ class _HomePageState extends State<HomePage> {
                       child: ListTile(
                         title: Text(items[index].title.toString()),
                         subtitle: Text(items[index].desc.toString()),
-                        trailing: IconButton(
-                          onPressed: () {
-                            box.deleteAt(index);
-                          },
-                          icon: const Icon(Icons.delete),
+                        trailing: SizedBox(
+                          width: 100,
+                          child: Row(
+                            children: [
+                              IconButton(
+                                onPressed: () {
+                                  String title = items[index].title.toString();
+                                  String desc = items[index].desc.toString();
+                                  updatePopup(title, desc);
+                                },
+                                icon: const Icon(Icons.edit),
+                              ),
+                              IconButton(
+                                onPressed: () {
+                                  box.deleteAt(index);
+                                },
+                                icon: const Icon(Icons.delete),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ),
